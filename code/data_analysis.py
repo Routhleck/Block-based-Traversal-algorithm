f7be0bdf9f3a0abfcfd19ecb25c8ac3bb4d72c36
Routhleck <1310722434@qq.com>
2023-06-21 13:54:55 +0800

    Update data_analysis.py

--- code/data_analysis.py
+++ code/data_analysis.py
@@ -2,14 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load data from csv
+# 读csv
 df = pd.read_csv('test.csv')
 
 # Preprocessing block_size and field_size to be integer tuples (using first value of tuple)
 df['block_size'] = df['block_size'].str.replace('(', '').str.replace(')', '').str.split(', ').apply(lambda x: int(x[0]))
 df['field_size'] = df['field_size'].str.replace('(', '').str.replace(')', '').str.split(', ').apply(lambda x: int(x[0]))
 
-# Group by field_size and plot
+# 按field_size和plot分组
 grouped = df.groupby('field_size')
 for name, group in grouped:
     plt.figure(figsize=(10, 6))
@@ -23,7 +23,7 @@ for name, group in grouped:
     plt.show()
 
 
-# Creating heatmaps
+# heatmap
 plt.figure(figsize=(10, 6))
 pivot_table = df.pivot(index='field_size', columns='block_size', values='time_traversal_parallel')
 sns.heatmap(pivot_table, annot=True, fmt=".1f", cmap='YlGnBu')
@@ -40,7 +40,7 @@ plt.xlabel('Block Size')
 plt.ylabel('Field Size')
 plt.show()
 
-# Creating boxplots
+# boxplot
 plt.figure(figsize=(10, 6))
 sns.boxplot(x='block_size', y='time_traversal_parallel', data=df)
 plt.title('Boxplot of Time Traversal Parallel vs Block Size')
